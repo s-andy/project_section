4186d76380d83314d51d1d9c741fadcdac9656cf
Andriy Lesyuk <s-andy@andriylesyuk.com>
2013-09-09 23:20:37 +0000

    Added Redmine 2.x routes
    git-svn-id: http://svn.s-andy.com/project-sections@2 519756ee-cd73-464a-a8f3-ca700de9cd38

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,18 @@ if Rails::VERSION::MAJOR < 3
         map.connect('sections/:id/update', :controller => 'project_sections', :action => 'update',  :conditions => { :method => :put })
         map.connect('sections/:id',        :controller => 'project_sections', :action => 'destroy', :conditions => { :method => :delete })
         map.connect('section/*section',    :controller => 'sections',         :action => 'index')
-
         map.connect('project/*section',    :controller => 'projects',         :action => 'show')
     end
 
 else
 
-    # TODO
+    match('sections',          :to => 'project_sections#index')
+    match('sections/new',      :to => 'project_sections#new')
+    post('sections/create',    :to => 'project_sections#create')
+    match('sections/:id/edit', :to => 'project_sections#edit')
+    put('sections/:id/update', :to => 'project_sections#update')
+    delete('sections/:id',     :to => 'project_sections#destroy')
+    match('section/*section',  :to => 'sections#index')
+    match('project/*section',  :to => 'projects#show')
 
 end
